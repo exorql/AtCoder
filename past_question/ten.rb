ac2c9a77eaf185867a8dac1fcc9230edd23e7af1
haruto.watanabe <w.haru10n@gmail.com>
2020-05-05 09:04:52 +0900

    Traveling

---
+++ past_question/ten.rb
@@ -0,0 +1,19 @@
+n = gets.to_i
+t1,x1,y1 = 0,0,0
+judge = "Yes"
+
+n.times do
+  s = gets
+  t2,x2,y2 =s.strip.split(" ").map(&:to_i)
+  t = t2 - t1
+  d = (x2 - x1).abs + (y2 - y1).abs
+  if d <= t && (t - d).even?
+    t1 = t2
+    x1 = x2
+    y1 = y2
+  else
+    judge = "No"
+    break
+  end
+end
+print judge
